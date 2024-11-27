dcc42b77b9e844e6ce2493f98ea9857bbab582a8
Bárbara <00001120834740sp@al.educacao.sp.gov.br>
2024-11-27 13:40:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sua última alteza',
+    'Fala sobre o que?',
+    'Millie Quint fica arrasada quando descobre que sua meio-que-melhor-amiga/meio-que-namorada tem beijado outra pessoa.'
+)
+
+criaCartao(
+    'Od dois morrem no final',
+    'Fala sobre o que?',
+    'as pessoas recebem uma ligação da Central da Morte avisando que, em algum momento das próximas 24 horas, a vida delas chegará ao fim.'
+)
+
+criaCartao(
+    'Evelyn Hugo',
+    'Fala sobre o que?',
+    'Evelyn Hugo era uma mulher bonita que, no contexto do século XX, se casou com sete homens diferentes. Infelizmente, assim como muitas outras mulheres dessa época, a atriz viveu relacionamentos tóxicos, sofrendo humilhações, dependências e agressões vindas de seu marido.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Olá, tudo bem? em Inglês?',
+    'Oi em ingles é Hello everything is fine?'
+)
